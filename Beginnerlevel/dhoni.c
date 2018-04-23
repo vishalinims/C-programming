b3359e06db21e42454ad72385c051ffb2390186d
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-04-23 09:51:58 +0000

    Create dhoni.c

---
+++ Beginnerlevel/dhoni.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+
+{
+   char[20];
+  
+   int a,c;
+
+   float f;
+   
+   printf("M.S.Dhoni);
+
+   printf("mahi");
+
+   a =18426;
+
+   f =44.83;
+   
+   c =463 ;
+
+   printf("%d",a);
+
+   printf("%f",f);
+   
+   printf("%d",c);
+
+   return 0;
+
+}
