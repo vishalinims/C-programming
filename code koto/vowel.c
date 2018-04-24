54e59ac3f80fd53cf74da19e99a080ffa36e53bd
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-04-24 10:02:11 +0000

    Create vowel.c

---
+++ code koto/vowel.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    char v;
+    scanf("%c",&v);
+    switch(v)
+    {
+      case 'a':
+            printf("Vowel");
+            break;
+      case 'e':
+            printf("Vowel");
+            break;
+      case 'i':
+            printf("Vowel");
+            break;        
+      case 'o':
+            printf("Vowel");
+            break;
+      case 'u':
+            printf("Vowel");
+            break;  
+      default:
+            printf("Constant");
+    }
+return 0;
+ }
