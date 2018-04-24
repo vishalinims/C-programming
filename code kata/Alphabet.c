456962f89f2c7b7ab0e86bd7a6f9b1fbc786453a
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-04-24 10:05:13 +0000

    Create Alphabet.c

---
+++ code kata/Alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+ {
+    char c;
+    scanf("%c",&c);
+    if(c>='a' && c<='z' || c>='A' && c<='Z')
+    {
+      printf("Alphabet");
+    }
+    else
+    {
+      printf("No");
+    }
+    return 0;
+ }
+  
