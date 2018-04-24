79f7c7915138184db0605709a99dddf3f2c52d9f
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-04-24 04:47:20 +0000

    Create calculator.c

---
+++ player level/calculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ int a=5,b=5;
+
+ int addition,subtraction,multiplication,division,modulo;
+
+ addition=a+b;
+ 
+ subtraction=a-b;
+ 
+ multiplication=a*b;
+ 
+ division=a/b;
+ 
+ modulo=a%b;
+
+ printf("%d\n",addition);
+ 
+ 
+printf("%d\n",subtraction);
+  
+   printf("%d\n",multiplication);
+   
+ printf("%d\n",division);
+ 
+  printf("%d\n",modulo);
+}
