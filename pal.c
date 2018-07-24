f0f858ebd2f5f5b058ca62b47a5e92f79f5dcd69
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-07-24 10:47:04 +0000

    Create pal.c

---
+++ pal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+    scanf("%d", &n);
+    originalInteger = n;
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
+    if (originalInteger == reversedInteger)
+        printf("yes", originalInteger);
+    else
+        printf("no", originalInteger);
+    
+    return 0;
+}
