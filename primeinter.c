46f76d91f09c9b9b679f6803964e52ba051676df
vishalinims <38653005+vishalinims@users.noreply.github.com>
2018-08-04 06:46:45 +0000

    Create primeinter.c

---
+++ primeinter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int n1, n2, i, j, flag;
+  scanf("%d %d", &n1, &n2);
+  printf("",n1, n2);
+  for(i=n1+1; i<n2; ++i)
+  {
+      flag=0;
+      for(j=2; j<=i/2; ++j)
+      {
+        if(i%j==0)
+        {
+          flag=1;
+          break;
+        }
+      }
+      if(flag==0)
+        printf("%d ",i);
+  }
+  return 0;
+}
